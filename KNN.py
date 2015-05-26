e00a147602244159b330aaef85c1e61d295c8c73
Aaron Karp <aaronkarp123@gmail.com>
2015-05-26 18:48:00 -0500

    Working KNN
    Finds 1NN (hamming distance) with the testFile

--- KNN.py
+++ KNN.py
@@ -9,7 +9,7 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	    dataset = list(lines)
 	    for x in range(len(dataset)-1):
 	    	if numLine > 0:
-		        for y in range(4):
+		        for y in range(len(dataset[x])):
 		            dataset[x][y] = float(dataset[x][y])
 		        if random.random() < split:
 		            trainingSet.append(dataset[x])
@@ -18,12 +18,30 @@ def loadDataset(filename, split, trainingSet=[] , testSet=[]):
 	    	numLine += 1
 
 def hammingDistance(instance1, instance2, length):
-	
+	distance = 0
+	for x in range(length):
+		if instance1[x] != instance2[2]:
+			distance += 1
+	return distance
 
-
+def findClosest(test, length):
+	closest = None
+	minDist = hammingDistance(test, trainingSet[0], len(trainingSet[0]))
+	for x in range(len(trainingSet)):
+		tempDist = hammingDistance(test, trainingSet[x], len(trainingSet[0]))
+		if tempDist < minDist:
+			minDist = tempDist
+			closest = trainingSet[x]
+	return closest, minDist
 
 trainingSet=[]
 testSet=[]
 loadDataset('./data/games_by_username.csv', 0.66, trainingSet, testSet)
 print 'Train: ' + repr(len(trainingSet))
 print 'Test: ' + repr(len(testSet))
+closestOne, dist = findClosest(testSet[0], len(trainingSet[0]))
+print '# Games: ',
+print len(trainingSet[0])
+print 'Distance: ' + repr(dist)
+print 'Closest Node: ',
+print closestOne
